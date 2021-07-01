20c3da6bf4b3887ac2bbe8df0924d0712a8586fc
Josue Lemus <user@itadmins-Mac-mini.local>
2021-07-01 14:36:55 -0400

    movie details

--- App.tsx
+++ App.tsx
@@ -1,15 +1,21 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
-
+import { StatusBar } from 'expo-status-bar';
 import MovieDetail from './MovieDetail';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+        <StatusBar style="light" />
         <MovieDetail
           rating={9.2}
-          peopleWatching={8200}
+          peopleWatching={5200}
+          urlImageBanner={'https://i.ytimg.com/vi/8GltZA7F7-M/maxresdefault.jpg'}
+          urlImageProfile={'https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/styles/cover_290x414/public/media/image/2019/02/rMEfTRA5BEbuVweTF1cQZiT7WdZ.jpg?itok=gfuWJ2F5'}
+          genders= {"Action, Adventure"}
+          title = {'Space Jam: A New Legacy (2021)'}
+          description = {`Space Jam: A New Legacy (also known as Space Jam 2) is an upcoming American live-action/animated sports comedy film directed by Malcolm D. Lee, serving as a standalone sequel to Space Jam (1996). It will mark the first theatrically-released film to feature the Looney Tunes characters since Looney Tunes: Back in Action (2003), and like the previous hybrid films, it will be a combination of live-action, traditional hand-drawn animation, and 3D CGI effects. The film stars basketball player LeBron James as a fictionalized version of himself along with Don Cheadle, Khris Davis, and Sonequa Martin-Green in live-action roles, while Jeff Bergman, Eric Bauza, and Zendaya headline the Looney Tunes voice cast.`}
         />
       </View>
     );
@@ -19,8 +25,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#fff'
   },
 });
